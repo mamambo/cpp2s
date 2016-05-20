cb31cd2c48d5161ba37cdc9652955b4014fdb6fd
mamambo <imyourmambo@gmail.com>
2016-05-20 20:12:03 +0300

    Add files via upload

---
+++ task1/main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(int args, char * argv[])
+{
+    int n;
+    scanf("%d", &n);
+    int** d = (int**)malloc((n) * sizeof(int*));
+    for (int i = 1; i <= n; i++) {
+        d[i] = (int*) malloc (n * sizeof(int));
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            d[i][j] = i * j;
+        }
+    }
+    int x1, y1, x2, y2;
+    scanf("%d", &x1);
+    if (x1 == 0) {
+        free(d);
+        return 0;
+    }
+    scanf("%d %d %d", &y1, &x2, &y2);
+    for (int i = x1; i <= x2; i++) {
+        for (int j = y1; j <= y2; j++) {
+            printf("%d ", d[i][j]);
+        }
+        printf("\n");
+    }
+    free(d);
+    return 0;
+}
